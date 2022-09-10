72021370562a3b47412de2615dcb3d91dbfffeb6
juanesherrera21 <juanesherrera21@hotmail.com>
2022-09-10 18:23:55 -0500

    Agregar archivos de proyecto.

---
+++ MVVMsample/MVVMsample/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using MVVMsample.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+
+namespace MVVMsample
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public Persona currentPersona { get; set; }
+        public MainPage()
+        {
+            InitializeComponent();
+            loadData();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Saving","Saving Data","Ok");
+
+        }
+
+        public void loadData() {
+            //connection  to web services
+
+            currentPersona = new Persona()
+            {
+                Name="Hector",Phone="000999",Addres="XXX"
+
+            };
+
+            txtName.Text = currentPersona.Name;
+
+            txtPhone.Text = currentPersona.Phone;
+
+            txtAddres.Text = currentPersona.Addres;
+        
+        }
+    }
+}
---
+++ MVVMsample/MVVMsample/Models/Persona.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MVVMsample.Models
+{
+    public class Persona
+    {
+        public string Name { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Addres { get; set; }
+
+
+    }
+}
